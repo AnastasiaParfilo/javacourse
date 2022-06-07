ac422fc3a1d307e15106550e413daf87d5d1b40d
Anastasia Parfilo <parfilo2003@gmail.com>
2022-06-07 22:00:58 +0300

    mult arrays & string

---
+++ mult_array/src/main/java/ua/com/course/MultArrayMain.java
@@ -0,0 +1,9 @@
+package ua.com.course;
+
+import java.io.IOException;
+
+public class MultArrayMain {
+    public static void main(String[] args) throws IOException {
+        RunProgram.start();
+    }
+}
